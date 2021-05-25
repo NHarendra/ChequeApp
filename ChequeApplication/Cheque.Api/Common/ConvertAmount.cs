e33b336cebfb4447e229e452adc51c990af89968
NHarendra <harendra@innolat.com>
2021-05-25 13:04:47 +0530

    web project

---
+++ ChequeApplication/Cheque.Api/Common/ConvertAmount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cheque.Api.Common
+{
+    public static class ConvertAmount
+    {
+
+    }
+}
